f0148ad5896dd14d11db43fcc1f9355ce6460260
Daniel Isaac Khan Ramiro <dkhanram@cisco.com>
2016-11-09 12:38:09 +0000

    Add non-interactive support to gpg command (batch)

--- travis-ci/before-deploy.sh
+++ travis-ci/before-deploy.sh
@@ -4,5 +4,5 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_363c33d4ecac_key -iv $encrypted_363c33d4ecac_iv -in travis-ci/codesigning.asc.enc -out travis-ci/signingkey.asc -d
-    gpg --yes --fast-import travis-ci/signingkey.asc
+    gpg --yes --batch --fast-import travis-ci/signingkey.asc
 fi
